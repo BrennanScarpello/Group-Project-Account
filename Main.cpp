6af53917e573680c2bb2e0443b269cd3947e84ea
BrennanScarpello <61513553+BrennanScarpello@users.noreply.github.com>
2020-02-27 10:37:23 -0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+/*This is the Main Test Program for the Github banking Project
+ABBA Team
+Period 3
+Febuary 27th 2020*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
